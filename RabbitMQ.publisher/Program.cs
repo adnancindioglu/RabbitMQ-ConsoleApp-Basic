7d685c4bc44dddb535f74b9ebcdaed3bfe2b0772
adnan cindioglu <adnancindioglu@hotmail.com>
2022-01-26 23:17:44 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
+
+            //bağlantı oluşturuyoruz
+            using var connection = factory.CreateConnection();
+
+            //kanal oluşturuyoruz
+            var channel = connection.CreateModel();
+
+            //queue:kanal adı
+            //durable:false->kuyruktaki verilen memory tutulur(uygulama durursa kaybolur)  true->kuyruktaki veriler fiziksel kayıt edilir(veriler kaybolmaz)
+            //exclusive:false-> kuyruğa farklı bir kanalla ulaşabilirsin true:kuyruğa sadece channel kanalıyla bağlanabilirsin
+            //autoDelete:true-> subscriber kapanırsa yada bağlantı koparsa kuyruğu siler
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            //kuyruğa 50 mesaj gönderelim
+            Enumerable.Range(1, 50).ToList().ForEach (x => 
+            {
+                string message = $"Message {x}";
+
+                byte[] messageBody = Encoding.UTF8.GetBytes(message);
+
+                //channel kanalıla gönderiyoruz
+                //exchange yoksa default exchange kullanılır ve routingKey kanal adı verilir
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine($"Mesajınız Gönderilmiştir: {message}");
+            });
+
+            
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //kanal publisher tarafında oluşturulduğu için burda oluşturmaya gerek kalmaz
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            //prefetchSize:alınacak mesajın boyutu 0-> herhangibir boyuttta gönderebilirsin
+            //prefetchCount:her bir subscriber kaç mesaj alsın
+            //global:true->prefetchCount sayıyı subscriber eşit şekilde paylaştırır false->her subscriber prefetchCount sayısında gönderim yapar
+            channel.BasicQos(0,1,false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            //autoAck:true-> message doğruda işlense yanlışta işlense kuyruktan siler false->mesajı işledikten sonra silmen için haberdar edeceğim
+            channel.BasicConsume("hello-queue",false,consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e)=> 
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine(message);
+
+                //ilgili mesaj işlenmişse kuyruktan sil
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+          
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
