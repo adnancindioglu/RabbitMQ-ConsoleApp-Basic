6af67227b8728125ffff5ba9337029539c53c3dd
adnan cindioglu <adnancindioglu@hotmail.com>
2022-01-29 00:40:28 +0300

    fanout exchamge and direct exchange

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -9,6 +9,13 @@ namespace RabbitMQ.publisher
     {
         static void Main(string[] args)
         {
+            NotExchange();
+            //FanoutExchange();
+            //DirectExchange();
+        }
+
+        private static void NotExchange()
+        {
             var factory = new ConnectionFactory();
 
             factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
@@ -26,7 +33,7 @@ namespace RabbitMQ.publisher
             channel.QueueDeclare("hello-queue", true, false, false);
 
             //kuyruğa 50 mesaj gönderelim
-            Enumerable.Range(1, 50).ToList().ForEach (x => 
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
                 string message = $"Message {x}";
 
@@ -39,11 +46,93 @@ namespace RabbitMQ.publisher
                 Console.WriteLine($"Mesajınız Gönderilmiştir: {message}");
             });
 
-            
 
-            
+
+
 
             Console.ReadLine();
+        }
+
+        private static void FanoutExchange()
+        {
+            //Aldığı mesajı tüm kuyruklara gönderir
+            //Queues'nin Subscriber tarafında oluşturulmasının sebebi Subscriber kapandımı mesajın kuyrukta kalmaması amaçlanır(seneryoya göre değişir)
+
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-fanout", type: ExchangeType.Fanout, durable: true);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"log {x}";
+
+                byte[] messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
+
+                Console.WriteLine($"Mesajınız Gönderilmiştir: {message}");
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void DirectExchange()
+        {
+            //publisher gelen mesajın route göre ilgili kuyruğa gönderir
+            //kuyruğu publisher tarafında oluşturacağız(seneryoya göre değişir)
+
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-direct", type: ExchangeType.Direct, durable: true);
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var routeKey = $"route-{x}";
+
+                var queueName = $"direct-queue-{x}";
+
+                channel.QueueDeclare(queueName, true, false, false);
+
+                channel.QueueBind(queueName, "logs-direct", routeKey, null);
+
+            });
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log = (LogNames)new Random().Next(1, 5);
+
+                string message = $"log-type: {log}";
+
+                var routeKey = $"route-{log}";
+
+                byte[] messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+                Console.WriteLine($"Log Gönderilmiştir: {message}");
+            });
+
+            Console.ReadLine();
+        }
+
+        public enum LogNames
+        {
+            Critical = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4
         }
     }
 }
--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -10,6 +10,13 @@ namespace RabbitMQ.subscriber
     {
         static void Main(string[] args)
         {
+            NotExchange();
+            //FanoutExchange();
+            //DirectExchange();
+        }
+
+        private static void NotExchange()
+        {
             var factory = new ConnectionFactory();
 
             factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
@@ -24,14 +31,14 @@ namespace RabbitMQ.subscriber
             //prefetchSize:alınacak mesajın boyutu 0-> herhangibir boyuttta gönderebilirsin
             //prefetchCount:her bir subscriber kaç mesaj alsın
             //global:true->prefetchCount sayıyı subscriber eşit şekilde paylaştırır false->her subscriber prefetchCount sayısında gönderim yapar
-            channel.BasicQos(0,1,false);
+            channel.BasicQos(0, 1, false);
 
             var consumer = new EventingBasicConsumer(channel);
 
             //autoAck:true-> message doğruda işlense yanlışta işlense kuyruktan siler false->mesajı işledikten sonra silmen için haberdar edeceğim
-            channel.BasicConsume("hello-queue",false,consumer);
+            channel.BasicConsume("hello-queue", false, consumer);
 
-            consumer.Received += (object sender, BasicDeliverEventArgs e)=> 
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
@@ -41,11 +48,93 @@ namespace RabbitMQ.subscriber
                 //ilgili mesaj işlenmişse kuyruktan sil
                 channel.BasicAck(e.DeliveryTag, false);
             };
-          
+
 
             Console.ReadLine();
         }
 
-        
+        private static void FanoutExchange()
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, false);
+
+            //kuyruğu subscriber tarafında oluştururuz
+            //kuyruk adı random olmalı çünkü farklı subscriberlar aynı kuyruğu dinlememeli
+            var randomQueueName = channel.QueueDeclare().QueueName;
+
+            //kalıcı kuyruk için
+            //kuyruğu kendimizde oluşturabiliriz randomQueueName belli bir isim vermemiz gerekir
+            //bu sayede kuyruktaki mesajlar kaybolmaz tekrar çalışınca kaldığı yerden devam eder ve yeni mesajlar kapalı olsa daki birikir
+            //***channel.QueueDeclare(randomQueueName, true, false, false);
+
+            //uygulama kapandığında kuyruğu silmemize yarar
+            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(randomQueueName, false, consumer);
+
+            Console.WriteLine("Logları dinliyorum...");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine(message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+            Console.ReadLine();
+        }
+
+        private static void DirectExchange()
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://pzzrnikj:ljZ9pbzuot5AdM2C4NSj3tdrTS1HcoLy@baboon.rmq.cloudamqp.com/pzzrnikj");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            //farklı kuyruklardaki mesajları alabiliriz
+            var queueName = "direct-queue-Critical";
+            //var queueName = "direct-queue-Error";
+            //var queueName = "direct-queue-Warning";
+            //var queueName = "direct-queue-Info";
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Logları dinliyorum...");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine(message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+            Console.ReadLine();
+        }
+
+
     }
 }
